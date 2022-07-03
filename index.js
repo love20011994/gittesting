cc42273c08d5ff827ea40b9f9c4230d63aad61da
love20011994 <love.kalsangrah@gmail.com>
2022-07-03 13:24:04 +0530

    ggggggdgggsdj

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const str ='google'
 const str1 = str.split('')
 
 const a = str1.filter((p,q,r)=>{
-      return r.indexOf(p)!==q
+      return r.indexOf(p)!==qhhhhh
 })
 const b = a.map((r,s)=>{
      if(str1.includes(r)){
